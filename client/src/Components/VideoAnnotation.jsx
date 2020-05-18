82aebe6256d284c0ae52f55c170f6fe9b0041bfd
Viresh Uberoy <vireshuberoy01@gmail.com>
2020-05-18 16:38:36 +0530

    added data insertion. defaultAnnotations not working

--- client/src/Components/VideoAnnotation.jsx
+++ client/src/Components/VideoAnnotation.jsx
@@ -12,6 +12,12 @@ export default class VideoAnnotation extends Component {
     this.width = 1920;
     this.height = 1080;
 
+    this.defaultAnnotations = undefined;
+
+    this.state = {
+      ready: false,
+    };
+
     this.options = {
       method: "POST",
       headers: {
@@ -20,16 +26,88 @@ export default class VideoAnnotation extends Component {
     };
   }
 
+  async componentDidMount() {
+    const response = await fetch("/getData");
+    const json = await response.json();
+
+    this.setState({ defaultData: json.data[1].drawnData.data }, () => {
+      // console.log(this.state.defaultData[0]);
+      this.defaultAnnotations = this.state.defaultData[0];
+      this.setState({
+        defaultAnnotations: this.defaultAnnotations,
+        ready: true,
+      });
+    });
+  }
+
   submit(e) {
-    console.log(e);
+    this.filterData(e);
+  }
+
+  filterData(data) {
+    this.insertVideoData(data.annotations);
+  }
+
+  async insertVideoData(data) {
+    console.log(data);
+    this.options.body = JSON.stringify({ data });
+
+    const response = await fetch("/insertData", this.options);
+    const res = await response.json();
+
+    if (res.message !== "Not Inserted") {
+      alert("Data Inserted");
+    } else {
+      alert("some problem");
+      console.log(res);
+    }
   }
 
   render() {
+    // const videoAnn = this.state.defAnn;
+    // const videoAnn = this.state.defAnn;
+    // console.log(this.state.defAnn);
+    // console.log(videoAnn);
+    console.log(this.state);
+
+    let oo = [
+      {
+        color: "rgba(0, 0, 0, 1)",
+        incidents: [
+          {
+            x: 184.25,
+            y: 80,
+            width: 99,
+            height: 105,
+            time: 0,
+            status: "Show",
+            id: "kacb6zeg",
+            name: "kacb6zeg",
+            label: "",
+          },
+        ],
+        childrenNames: [],
+        parentName: "",
+        id: "kacb6zeg",
+        name: "kacb6zeg",
+        label: "1",
+      },
+    ];
+
     return (
       <div>
         <h1>Main Implementation</h1>
-
-        <TwoDimensionalVideo url={video} onSubmit={(e) => this.submit(e)} />
+        <TwoDimensionalVideo
+          url={video}
+          defaultAnnotations={oo}
+          onSubmit={(e) => this.submit(e)}
+        />
+        <TwoDimensionalVideo
+          url={video}
+          defaultAnnotations={
+            this.state.ready ? this.state.defaultAnnotations : []
+          }
+        />
       </div>
     );
   }
