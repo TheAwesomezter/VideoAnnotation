6fc7f28be35d70665a469a29e3e87ce0b9b823fe
Viresh Uberoy <vireshuberoy01@gmail.com>
2020-05-25 22:38:38 +0530

    using hashrouter instead of browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import VideoAnnotation from "./Components/VideoAnnotation";
 import VideoAnnotationHome from "./Components/VideoAnnotationHome";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
